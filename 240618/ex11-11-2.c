793e24c4ecce32a27f6892c1226c2444f7bf8b2f
ohsunny0402 <dsmdhgusrb@dsm.hs.kr>
2024-06-25 09:57:54 +0900

    240618

---
+++ 240618/ex11-11-2.c
@@ -0,0 +1,38 @@
+ #include <sys/types.h>
+ #include <unistd.h>
+ #include <stdio.h>
+ #include <stdlib.h>
+ #include <sys/wait.h>
+ #include <string.h>
+ 
+ int main(){
+         char *msg[3] = {"apple id red\n", "banana is yellow\n", "cherry is red\n"};
+         int p[2];
+         pid_t pid;
+         int cnt;
+
+         if(pipe(p) == -1){
+                 printf("fall to call pipe()\n");
+                 exit(1);
+         }
+         if((pid = fork()) == -1){
+                 printf("fail to call fork()\n");
+                 exit(1);
+         }
+         else if(pid > 0) {
+                printf("[parent]\n");
+                close(p[0]);
+		dup2(p[1],1); 
+                execlp("ls", "ls", "-al", (char *)0);
+		printf("[parent] fail to call execlp()\n");
+         }
+         else {
+                 printf("[chile]\n");
+                 close(p[1]);
+                 dup2(p[0], 0);
+                 execlp("wc", "wc", (char *)0);
+                 printf("[child] fail to call execlp()\n");
+ 
+         }
+}
+
